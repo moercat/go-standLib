0c1170c57d535a9fe17ff1dfab519bfd253dfa46
hongyi.lin <hongyi.lin@baishan.com>
2025-06-05 14:32:28 +0800

    变更

--- live_templates/1-old.go
+++ live_templates/1-old.go
@@ -1,24 +1,6 @@
 package live_templates
 
 func Old1() {
-	// 实时模版 循环 forr
-
-	var m = make(map[string]int)
-	//for i, i2 := range collection {
-	//
-	//}
-	// 实时模版 循环 fori
-	//for i := 0; i < ; i++ {
-	//
-	//}
-	// 实时模版 错误处理 err
-	//if err != nil {
-	//
-	//}
-
-	for s, i := range m {
-
-	}
 }
 
 func Old2() {
